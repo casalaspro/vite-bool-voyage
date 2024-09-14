8ea0c0a10c841e4fa6fe48cdcdf8e97db45332e5
casalaspro <casalaspro.alessandro@gmail.com>
2024-09-14 10:25:46 +0200

    REFACTORY

--- src/pages/AppTripCreate.vue
+++ src/pages/AppTripCreate.vue
@@ -3,19 +3,26 @@
     <h3 class="section-title mb-3">TRIPS</h3>
     <div class="card">
       <div class="card-body">
-        <form>
-          <div class="form-group">
+        <form class="trip_form">
+          <div class="form-group mb-3">
             <label for="trip_title">Trip title</label>
-            <input type="text" class="form-control" id="trip_title" aria-describedby="emailHelp" placeholder="Enter title">
+            <input type="text" class="form-control" id="trip_title" aria-describedby="Input for trip title" placeholder="Title">
             <small id="titleHelp" class="form-text text-muted">Enter a title for your trip.</small>
           </div>
-          <div class="form-group">
-            <label for="trip_description">Password</label>
-            <input type="password" class="form-control" id="trip_description" placeholder="Password">
-            <small id="descriptionHelp" class="form-text text-muted">Enter a description for your trip.</small>
+
+          <div class="form-group mb-3">
+            <label for="trip_description" class="form-label">Example textarea</label>
+             <textarea class="form-control" id="trip_description" rows="3"></textarea>
+             <small id="descriptionHelp" class="form-text text-muted">Enter a description for your trip.</small>
           </div>
 
-          <div class="accordion" id="accordionNations">
+          <div class="form-group mb-3">
+            <label for="trip_motto">Motto</label>
+            <input type="text" class="form-control" id="trip_motto" placeholder="Motto">
+            <small id="mottoHelp" class="form-text text-muted">Enter a motto if you want.</small>
+          </div>
+
+          <div class="accordion mb-3" id="accordionNations">
             <div class="accordion-item">
               <h2 class="accordion-header" id="headingOne">
                 <button @click="accordionToggle" class="accordion-button" :class="accordion == false ? 'collapsed' : ''" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
@@ -24,7 +31,6 @@
               </h2>
               <div id="nationsAccordion" class="accordion-collapse collapse" :class="accordion == true ? 'show' : ''"  aria-labelledby="headingOne"           data-bs-parent="#accordionNations">
                 <div class="accordion-body">
-                  <p>Choose the nations of your trip</p>
                   <div class="row">
                     <div v-for="nation in nations" class="form-check col-sm-12 col-md-6 col-lg-6 col-xl-3">
                     <input type="checkbox" name="alpha_3" class="form-check-input" :id="'alpha_3_'+nation.id">
@@ -34,9 +40,10 @@
                 </div>
               </div>
             </div>
+            <small id="titleHelp" class="form-text text-muted">Choose at least one country for your trip.</small>
           </div>
           <!-- <p v-html="nations" class="error"></p> -->
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit" class="btn btn_submit">Submit</button>
         </form>
       </div>
     </div>
@@ -68,6 +75,10 @@ export default {
         console.log(res);
         this.nations = res.data;
       })
+    },
+    sendTrip(){
+      let form = document.querySelector('.trip_form');
+      form.preve
     }
     
   },
@@ -86,6 +97,10 @@ export default {
     color: black;
   }
 
+  .btn_submit{
+    background-color: #f78fad;
+  }
+
   .accordion-button.collapsed {
   background: white;
   color: #585C5F;
@@ -94,5 +109,11 @@ export default {
 .accordion-button:focus {
     box-shadow: none;
     border-color: rgba(0,0,0,.125);
+}
+
+.nationHelp{
+  color: #585c5f;
+  border-bottom: 1px solid #585c5f2a;
+  font-size: 14px;
 }
 </style>
